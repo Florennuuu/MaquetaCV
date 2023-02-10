4c6b37392485103d2c0f37f53208728383e3e269
Florennuuu <55895692+Florennuuu@users.noreply.github.com>
2023-02-10 00:15:15 -0300

    index, styles and js

---
+++ js.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById('boton').addEventListener('click',function(){
+console.log('holamundo')
+});
+
+document.getElementById('boton_color').addEventListener('click',function(){
+    console.log('botoncolor')
+    });
+    
+document.getElementById('ocultar_info').addEventListener('click',function(){
+    document.getElementById('info').style.display = 'none';
+    });
+    
